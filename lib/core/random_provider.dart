473cd7f3f71151f03c66452b9743734d7f7fc3c8
i-sifat <isifat011@gmail.com>
2026-08-03 01:26:17 +0600

    chore: remove dead code and unused generated artifacts

--- lib/core/random_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:math';
-
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-/// Shared injectable random source for deterministic game tests.
-final randomProvider = Provider<Random>((ref) => Random.secure());
